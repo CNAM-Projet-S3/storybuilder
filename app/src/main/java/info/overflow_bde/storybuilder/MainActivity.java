0e91e725a783111307a1d93dec59109a5dd2d3ea
Léo Bergerot <leo.bergerot@gmail.com>
2019-10-30 12:27:21 +0100

    add event on main activity

--- app/src/main/java/info/overflow_bde/storybuilder/MainActivity.java
+++ app/src/main/java/info/overflow_bde/storybuilder/MainActivity.java
@@ -2,13 +2,38 @@ package info.overflow_bde.storybuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    final int PICTURE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, PICTURE_REQUEST);
+    }
+
+    public void choicePicture(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICTURE_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PICTURE_REQUEST) {
+            System.out.println("image was taken or choose");
+            //Bitmap image = (Bitmap) data.getExtras().get("data");
+            //ImageView imageview = (ImageView) findViewById(R.id.ImageView01);
+            //imageview.setImageBitmap(image);
+        }
     }
 }

ad29413b47fb83baf3d8b10c066776b2cbd96c39
Léo Bergerot <leo.bergerot@gmail.com>
2019-10-31 16:07:02 +0100

    set image view from take photo or choose photo action

--- app/src/main/java/info/overflow_bde/storybuilder/MainActivity.java
+++ app/src/main/java/info/overflow_bde/storybuilder/MainActivity.java
@@ -7,10 +7,14 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ImageView;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    final int PICTURE_REQUEST = 1;
+    final int PICTURE_TAKEN = 1;
+    final int PICTURE_CHOSEN = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,22 +24,33 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCamera(View view) {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, PICTURE_REQUEST);
+        startActivityForResult(intent, PICTURE_TAKEN);
     }
 
     public void choicePicture(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(intent, PICTURE_REQUEST);
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intent, PICTURE_CHOSEN);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == PICTURE_REQUEST) {
-            System.out.println("image was taken or choose");
-            //Bitmap image = (Bitmap) data.getExtras().get("data");
-            //ImageView imageview = (ImageView) findViewById(R.id.ImageView01);
-            //imageview.setImageBitmap(image);
-            Intent intent = new Intent(this, EditorActivity.class);
-            startActivity(intent);
+        Intent intent = new Intent(this, EditorActivity.class);
+        if (requestCode == PICTURE_TAKEN) {
+            System.out.println("image was taken");
+            Bitmap image = (Bitmap) data.getExtras().get("data");
+            try {
+                String uri = getCacheDir().getAbsolutePath() + "/tmpImage";
+                FileOutputStream out = new FileOutputStream(uri);
+                image.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.close();
+                intent.putExtra("Uri", uri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        if (requestCode == PICTURE_CHOSEN) {
+            System.out.println("image was choosen");
+            intent.putExtra("Uri", Objects.requireNonNull(data.getData()).toString());
+        }
+        startActivity(intent);
     }
 }

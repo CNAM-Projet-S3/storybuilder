67c8121e5992c23bfdb78e77c44648c174a99353
vois0004 <voisincassandre@gmail.com>
2020-01-05 21:43:24 +0100

    text layer

--- app/src/main/java/info/overflow_bde/storybuilder/MenuFragment.java
+++ app/src/main/java/info/overflow_bde/storybuilder/MenuFragment.java
@@ -14,89 +14,104 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.Objects;
+
 public class MenuFragment extends Fragment {
 
-    private ExportFragment      exportFragment;
-    private FragmentTransaction fragmentTransaction;
-    private StickersFragment    stickersFragment;
+	private ExportFragment      exportFragment;
+	private FragmentTransaction fragmentTransaction;
+	private StickersFragment    stickersFragment;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.menu_editor_fragment, container, false);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.menu_editor_fragment, container, false);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        this.fragmentTransaction = fragmentManager.beginTransaction();
+		FragmentManager fragmentManager = getFragmentManager();
+		this.fragmentTransaction = fragmentManager.beginTransaction();
 
-        this.shareOrSaveActionMenuButton(view);
-        this.exitMenuActionButton(view);
-        this.stickersMenuActionButton(view);
+		this.shareOrSaveActionMenuButton(view);
+		this.exitMenuActionButton(view);
+		this.stickersMenuActionButton(view);
+		this.textsMenuActionButton(view);
+		//hidden action menu
+		container.findViewById(R.id.editor_content).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stickersFragment.hidden();
+				exportFragment.hidden();
+			}
+		});
+		this.fragmentTransaction.commit();
 
-        //hidden action menu
-        container.findViewById(R.id.editor_content).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stickersFragment.hidden();
-                exportFragment.hidden();
-            }
-        });
-        this.fragmentTransaction.commit();
+		return view;
+	}
 
-        return view;
-    }
+	private void shareOrSaveActionMenuButton(View view) {
+		this.exportFragment = new ExportFragment();
+		this.fragmentTransaction.add(R.id.editor_fragment, exportFragment, "shareOrSave");
+		Button buttonShareOrSave = (Button) view.findViewById(R.id.save_or_share);
+		buttonShareOrSave.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				exportFragment.show();
+			}
+		});
+	}
 
-    private void shareOrSaveActionMenuButton(View view) {
-        this.exportFragment = new ExportFragment();
-        this.fragmentTransaction.add(R.id.editor_fragment, exportFragment, "shareOrSave");
-        Button buttonShareOrSave = (Button) view.findViewById(R.id.save_or_share);
-        buttonShareOrSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                exportFragment.show();
-            }
-        });
-    }
+	private void exitMenuActionButton(View view) {
+		Button buttonExit = (Button) view.findViewById(R.id.action_menu_exit);
+		buttonExit.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Intent intent = getActivity().getIntent();
+				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
+				getActivity().overridePendingTransition(0, 0);
+				getActivity().finish();
+				getActivity().overridePendingTransition(0, 0);
+				startActivity(intent);
+			}
+		});
+	}
 
-    private void exitMenuActionButton(View view) {
-        Button buttonExit = (Button) view.findViewById(R.id.action_menu_exit);
-        buttonExit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = getActivity().getIntent();
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                getActivity().overridePendingTransition(0, 0);
-                getActivity().finish();
-                getActivity().overridePendingTransition(0, 0);
-                startActivity(intent);
-            }
-        });
-    }
-
-    private void stickersMenuActionButton(View view) {
-        this.stickersFragment = new StickersFragment();
-        this.fragmentTransaction.add(R.id.editor_fragment, this.stickersFragment, "stickers");
-        FloatingActionButton buttonStickers = (FloatingActionButton) view.findViewById(R.id.editor_sticker);
+	private void stickersMenuActionButton(View view) {
+		this.stickersFragment = new StickersFragment();
+		this.fragmentTransaction.add(R.id.editor_fragment, this.stickersFragment, "stickers");
+		FloatingActionButton buttonStickers = (FloatingActionButton) view.findViewById(R.id.editor_sticker);
 
 
-        buttonStickers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                stickersFragment.show();
-            }
-        });
-    }
+		buttonStickers.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				stickersFragment.show();
+			}
+		});
+	}
 
-    public void show() {
-        FragmentManager fm = getFragmentManager();
-        fm.beginTransaction()
-                .setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
-                .show(this)
-                .commit();
-    }
 
-    public void hidden() {
-        FragmentManager fm = getFragmentManager();
-        fm.beginTransaction()
-                .hide(this)
-                .commit();
-    }
+	private void textsMenuActionButton(View view) {
+
+		FloatingActionButton buttonText = view.findViewById(R.id.editor_text);
+		buttonText.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				((MainActivity) Objects.requireNonNull(getActivity())).showFragment(new TextFragment(), R.id.editor_content, "text");
+
+			}
+		});
+	}
+
+	public void show() {
+		FragmentManager fm = getFragmentManager();
+		fm.beginTransaction()
+				.setCustomAnimations(android.R.anim.fade_in, android.R.anim.fade_out)
+				.show(this)
+				.commit();
+	}
+
+	public void hidden() {
+		FragmentManager fm = getFragmentManager();
+		fm.beginTransaction()
+				.hide(this)
+				.commit();
+	}
 }
---
+++ app/src/main/java/info/overflow_bde/storybuilder/TextFragment.java
@@ -0,0 +1,106 @@
+package info.overflow_bde.storybuilder;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
+public class TextFragment extends Fragment {
+	private String text;
+	private int    size;
+
+	private int yDelta;
+	private int xDelta;
+
+	private MenuFragment menuFragment;
+	private ViewGroup    mainLayout;
+	private EditText     editText;
+
+	public TextFragment() {
+		this.size = 20;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		View view = inflater.inflate(R.layout.text_fragment, container, false);
+		editText = view.findViewById(R.id.layer_text);
+		editText.setHint("TEXTE TEXTE");
+		editText.setTextSize(this.size);
+		mainLayout = (RelativeLayout) container.findViewById(R.id.editor_content);
+		menuFragment = (MenuFragment) Objects.requireNonNull(this.getFragmentManager()).findFragmentByTag("menu");
+
+		mainLayout.setOnClickListener(onClickListener());
+		view.setOnTouchListener(onTouchListener());
+		return view;
+	}
+
+	private View.OnTouchListener onTouchListener() {
+
+		return new View.OnTouchListener() {
+
+			@SuppressLint("ClickableViewAccessibility")
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+				final int x = (int) event.getRawX();
+				final int y = (int) event.getRawY();
+
+				switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+					case MotionEvent.ACTION_DOWN:
+						menuFragment.hidden();
+						RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+								view.getLayoutParams();
+
+						xDelta = x - lParams.leftMargin;
+						yDelta = y - lParams.topMargin;
+						break;
+					case MotionEvent.ACTION_UP:
+						menuFragment.show();
+						break;
+					case MotionEvent.ACTION_MOVE:
+						menuFragment.hidden();
+						RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view
+								.getLayoutParams();
+						layoutParams.leftMargin = x - xDelta;
+						layoutParams.topMargin = y - yDelta;
+						layoutParams.rightMargin = 0;
+						layoutParams.bottomMargin = 0;
+						view.setLayoutParams(layoutParams);
+						break;
+				}
+				return true;
+			}
+		};
+	}
+
+	private View.OnClickListener onClickListener() {
+		return new View.OnClickListener() {
+
+			@SuppressLint("ClickableViewAccessibility")
+			@Override
+			public void onClick(View view) {
+				editText.clearFocus();
+				InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+				imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+			}
+		};
+	}
+
+
+}
